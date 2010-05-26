bfc009f1b01831480a280de20fec32db561eefb2
Roban Hultman Kramer <roban@astro.columbia.edu>
2010-05-26 15:48:43 +0200

    Modified cosmolopy __init__.py to do some imports and defined a fiducial cosmo. 	modified:   cosmolopy/__init__.py

--- cosmolopy/__init__.py
+++ cosmolopy/__init__.py
@@ -10,6 +10,9 @@ The parameters module supplies some convenient pre-defined parameter sets.
 Examples:
 --------
 
+Calculate the mass of a halo with Virial temperature of 10^4 kelvin,
+then verify the Virial temperature for a halo of that mass.
+
 >>> import cosmolopy.perturbation as cp
 >>> cosmo = {'omega_M_0' : 0.27, 
 ... 'omega_lambda_0' : 1-0.27, 
@@ -27,4 +30,19 @@ Mass = 1.68e+08 M_sun
 >>> print round(temp, 4)
 10000.0
 
+Calculate the critical and matter densities.
+
+>>> from cosmolopy import *
+>>> 'rho_crit=%.3g Msun, rho_0=%.3g Msun' % cden.cosmo_densities(**fidcosmo)
+'rho_crit=1.38e+11 Msun, rho_0=3.75e+10 Msun'
+
 """
+
+import constants as cc
+import density as cden
+import distance as cd
+import perturbation as cp
+import reionization as cr
+import parameters as cparam
+
+fidcosmo = cparam.WMAP7_BAO_H0_mean(flat=True, extras=True)
