f76af3f4800b23c829c4aa3d3db94bfc61085bdd
Roban Hultman Kramer <robanhk@gmail.com>
2012-03-27 20:50:00 -0400

    Exclude swig-generated stuff from docs. 	modified:   make_docs.bash 	new file:   www/docAPI/_tf_fit'-module.html 	modified:   www/docAPI/api-objects.txt 	modified:   www/docAPI/class-tree.html 	modified:   www/docAPI/cosmolopy-module.html 	modified:   www/docAPI/cosmolopy-pysrc.html 	modified:   www/docAPI/cosmolopy.EH-module.html 	modified:   www/docAPI/cosmolopy.EH-pysrc.html 	new file:   www/docAPI/cosmolopy.EH.tf_fit-module.html 	new file:   www/docAPI/cosmolopy.EH.tf_fit-pysrc.html 	modified:   www/docAPI/cosmolopy.constants-module.html 	modified:   www/docAPI/cosmolopy.constants-pysrc.html 	modified:   www/docAPI/cosmolopy.density-module.html 	modified:   www/docAPI/cosmolopy.density-pysrc.html 	modified:   www/docAPI/cosmolopy.distance-module.html 	modified:   www/docAPI/cosmolopy.distance-pysrc.html 	modified:   www/docAPI/cosmolopy.luminosityfunction-module.html 	modified:   www/docAPI/cosmolopy.luminosityfunction-pysrc.html 	modified:   www/docAPI/cosmolopy.luminosityfunction.BrokenPowerlawSED-class.html 	modified:   www/docAPI/cosmolopy.luminosityfunction.LFHistory-class.html 	modified:   www/docAPI/cosmolopy.magnitudes-module.html 	modified:   www/docAPI/cosmolopy.magnitudes-pysrc.html 	modified:   www/docAPI/cosmolopy.parameters-module.html 	modified:   www/docAPI/cosmolopy.parameters-pysrc.html 	modified:   www/docAPI/cosmolopy.perturbation-module.html 	modified:   www/docAPI/cosmolopy.perturbation-pysrc.html 	modified:   www/docAPI/cosmolopy.reionization-module.html 	modified:   www/docAPI/cosmolopy.reionization-pysrc.html 	modified:   www/docAPI/cosmolopy.saveable-module.html 	modified:   www/docAPI/cosmolopy.saveable-pysrc.html 	modified:   www/docAPI/cosmolopy.saveable.NullWriter-class.html 	modified:   www/docAPI/cosmolopy.saveable.Saveable-class.html 	modified:   www/docAPI/cosmolopy.utils-module.html 	modified:   www/docAPI/cosmolopy.utils-pysrc.html 	modified:   www/docAPI/cosmolopy.utils.AgeSpacedRedshift-class.html 	modified:   www/docAPI/cosmolopy.utils.Extrapolate1d-class.html 	modified:   www/docAPI/cosmolopy.utils.Normalize-class.html 	modified:   www/docAPI/cosmolopy.utils.PiecewisePowerlaw-class.html 	modified:   www/docAPI/help.html 	modified:   www/docAPI/identifier-index.html 	modified:   www/docAPI/index.html 	modified:   www/docAPI/module-tree.html

--- make_docs.bash
+++ make_docs.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-epydoc --exclude='cosmolopy.EH._power' --exclude='cosmolopy.EH.power' -v --no-private --no-frames --html --docformat restructuredtext cosmolopy/ -o www/docAPI/
+epydoc --exclude='cosmolopy.EH._power' --exclude='cosmolopy.EH._tf_fit' --exclude='cosmolopy.EH.power' --exclude='cosmolopy.EH.tf_fit' -v --no-private --no-frames --html --docformat restructuredtext cosmolopy/ -o www/docAPI/
